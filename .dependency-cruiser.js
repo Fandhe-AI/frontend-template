4c3ea33d39823bf6613850d343ca6d75045971d8
aLiz-Nancy <nancy@alizlab.com>
2025-07-02 01:48:45 +0900

    feat(config): commitlintとlinterの設定ファイルを追加し、依存関係を更新

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   extends: [
     "@repo/shared-config-dependency-cruiser/configs/core",
     "@repo/shared-config-dependency-cruiser/configs/typescript",
---
+++ commitlint.config.ts
@@ -0,0 +1,5 @@
+import type { UserConfig } from "@commitlint/types";
+
+export default {
+  extends: "@repo/shared-config-commitlint/configs/base",
+} satisfies UserConfig;
